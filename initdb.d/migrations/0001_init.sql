35e23a049e2b6590442230ae7703e4f7ea3d72ad
saufiroja <saufiroja08@gmail.com>
2023-10-08 20:29:08 +0700

    refactor: delete role

--- initdb.d/migrations/0001_init.sql
+++ initdb.d/migrations/0001_init.sql
@@ -1,5 +1,5 @@
 -- Buat user 'root' jika belum ada
-CREATE USER root WITH PASSWORD 'root' SUPERUSER;
+CREATE USER root WITH PASSWORD 'root';
 
 -- Berikan semua hak akses ke database 'boilerplate' kepada user 'root'
 GRANT ALL PRIVILEGES ON DATABASE boilerplate TO root;
